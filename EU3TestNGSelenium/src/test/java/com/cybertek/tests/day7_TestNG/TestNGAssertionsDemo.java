d18eb6d86bbd4746ff98df2b32b1960b40ed7ee5
Kadircan162 <kadircangulde@gmail.com>
2021-12-15 01:13:25 +0300

    Last

--- EU3TestNGSelenium/src/test/java/com/cybertek/tests/day7_TestNG/TestNGAssertionsDemo.java
+++ EU3TestNGSelenium/src/test/java/com/cybertek/tests/day7_TestNG/TestNGAssertionsDemo.java
@@ -1,11 +1,15 @@
 package com.cybertek.tests.day7_TestNG;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestNGAssertionsDemo {
+
+    @BeforeClass
+    public void testBefore(){
+        System.out.println("----BEFORE CLASS----");
+        System.out.println("Executed one time before anything in class");
+    }
 
     @BeforeMethod
     public void openBrowser(){
@@ -27,12 +31,18 @@ public class TestNGAssertionsDemo {
 
     @Test
     public void test2(){
-        System.out.println("Second assertion");
+        System.out.println("Fourth assertion");
         Assert.assertEquals("apple", "apple");
     }
 
     @AfterMethod
     public void closeBrowser(){
         System.out.println("Close the browser");
+    }
+
+    @AfterClass
+    public void testAfterClass(){
+        System.out.println("----AFTER CLASS----");
+        System.out.println("Some reporting code here..");
     }
 }
