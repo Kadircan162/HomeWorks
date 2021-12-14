24eb04d2cf2aa15ca837dcc75da9f41430beaefb
Kadircan162 <kadircangulde@gmail.com>
2021-12-15 02:12:12 +0300

    Last

--- EU3TestNGSelenium/src/test/java/com/cybertek/tests/day7_TestNG/TestNGAssertionsDemo.java
+++ EU3TestNGSelenium/src/test/java/com/cybertek/tests/day7_TestNG/TestNGAssertionsDemo.java
@@ -35,6 +35,39 @@ public class TestNGAssertionsDemo {
         Assert.assertEquals("apple", "apple");
     }
 
+    @Test
+    public void test3(){
+        String expectedTitle = "Cyb";
+        String actualTitle = "Cybertek";
+        Assert.assertEquals(actualTitle.substring(0, 3), expectedTitle);
+        System.out.println("TEST3: PASS");
+        Assert.assertTrue(actualTitle.startsWith(expectedTitle), "Verify title starts with Cyb");
+        System.out.println("TEST3: PASS");
+
+        //Here no difference between two assert statements as both verify if actualTitle starts with expectedTitle
+        //Both statement gets executed
+    }
+
+    @Test
+    public void test4(){
+        //verify if email contains @ sign
+        String email = "mikesmith@gmail.com";
+        Assert.assertTrue(email.contains("@"), "verify email contains @ sign");
+        System.out.println("TEST4: PASS");
+
+    }
+
+    @Test
+    public void test5(){
+        //Assert.assertTrue(0>1, "verify 0 is greater than 1");
+        Assert.assertFalse(0>1, "verify 0 is not greater than 1");
+    }
+
+    @Test
+    public void test6(){
+        Assert.assertNotEquals("test", "test");
+    }
+
     @AfterMethod
     public void closeBrowser(){
         System.out.println("Close the browser");
